7570300f47fef7fc77b557fe1b5e6a9c107b0fbe
K-Yaman0 <tezza10yama@gmail.com>
2023-10-13 11:05:15 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday].each do |v|
+          validates v, presence: true
+   end
 end
