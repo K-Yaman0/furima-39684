d5f40c30b1b75ea3f03ad9c9c1511951907cdafa
K-Yaman0 <tezza10yama@gmail.com>
2023-10-15 16:46:44 +0900

    テスト項目洗い出し

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nickname、email、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthdayが存在すれば登録できる' do
+
+      end
+    end
+    context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+      end
+      it 'emailが空では登録できない' do
+      end
+      it 'passwordが空では登録できない' do
+      end
+      it 'last_nameが空では登録できない' do
+      end
+      it 'first_nameが空では登録できない' do
+      end
+      it 'last_name_kanaが空では登録できない' do
+      end
+      it 'first_name_kanaが空では登録できない' do
+      end
+      it 'birthdayが空では登録できない' do
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+      end
+      it 'nicknameが40文字以上では登録できない' do
+      end
+      it '重複したemailが存在する場合は登録できない' do
+      end
+      it 'emailは@を含まないと登録できない' do
+      end
+      it 'passwordが5文字以下では登録できない' do
+      end
+      it 'passwordが129文字以上では登録できない' do
+      end
+      it 'お名前(全角)は半角では登録できない' do
+      end
+      it 'お名前カナ(全角)は全角カタカナ以外では登録できない' do
+      end
+    end
+  end
 end
