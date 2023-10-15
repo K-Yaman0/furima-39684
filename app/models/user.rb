3b791c125a7dfb3951782d6825df3498fc99ff93
K-Yaman0 <tezza10yama@gmail.com>
2023-10-15 23:16:47 +0900

    バリデーションに『ー』追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     validates v, presence: true
   end
 
-  validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'Input full-width characters'
+  validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'Input full-width characters'
   validates_format_of :last_name_kana, :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'Input full-width katakana characters'
 
   validates :nickname, length: { maximum: 40 }
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { a+ Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name             { '田なカ' }
     first_name            { '次ろウ' }
