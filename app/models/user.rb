9006ede833f85428b44acc8640e2cabe7a85ce3a
K-Yaman0 <tezza10yama@gmail.com>
2023-10-15 14:28:16 +0900

    お名前バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ApplicationRecord
   
   [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday].each do |v|
           validates v, presence: true
-   end
+  end
+  # validates 記述する お名前→全角 お名前(カナ)→カタカナ、全角 
+  validates_format_of :last_name, :first_name, with:/\A[ぁ-んァ-ン一-龥々]+\z/, message: "Input full-width characters"
+  validates_format_of :last_name_kana, :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: "Input full-width katakana characters"
+
   validates :nickname, length: { maximum: 40 }
   validates :password, length: { minimum: 6 }
+ 
 end
