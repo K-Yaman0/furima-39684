e185651564769a5601308cdefae2cb789dfded58
K-Yaman0 <tezza10yama@gmail.com>
2023-10-16 23:21:53 +0900

    パスワード英数混合バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
     validates v, presence: true
   end
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
   validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'Input full-width characters'
   validates_format_of :last_name_kana, :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'Input full-width katakana characters'
 
