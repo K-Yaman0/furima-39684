8c934e8504c1b2497c0161712f7c717c84cc44bc
K-Yaman0 <tezza10yama@gmail.com>
2023-10-17 18:28:04 +0900

    last_name, first_nameバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'Input full-width characters'
+  validates_format_of :last_name, :first_name, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters'
   validates_format_of :last_name_kana, :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'Input full-width katakana characters'
 
   validates :nickname, length: { maximum: 40 }
