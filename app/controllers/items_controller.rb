343df3edebfd1673bb256bf5628851572d9e11ac
K-Yaman0 <tezza10yama@gmail.com>
2023-10-18 11:18:48 +0900

    出品内容を保存できるよう記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,20 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def item_params
+    params.rwquire(:item).permit(:image, :item_name, :description, :category_id, :item_condition_id, :freight_id, :prefecture_id, :ship_date_id, :price)
   end
 end
