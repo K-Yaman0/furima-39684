a7f135363e58f089c07bc2d7d265be529532d87b
K-Yaman0 <tezza10yama@gmail.com>
2023-10-26 09:59:40 +0900

    imageテスト修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image               {Faker::Lorem.sentence}
     item_name           {Faker::Name.name }
     description         {Faker::Lorem.sentence}
     category_id         {Faker::Number.between(from: 1, to: 10)}
@@ -10,5 +9,10 @@ FactoryBot.define do
     ship_date_id        {Faker::Number.between(from: 1, to: 3)}
     price               {Faker::Number.between(from: 300, to: 9_999_999)}
     association :user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    
   end
 end
