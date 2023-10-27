b5d36cad597005bb7151e811687451ef8a185e27
K-Yaman0 <tezza10yama@gmail.com>
2023-10-27 14:52:40 +0900

    rubocop 自動修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,64 +28,64 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'category_idが0では出品できない' do
-        @item.category_id= 0
+        @item.category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it 'item_condition_idが0では出品できない' do
-        @item.item_condition_id= 0
+        @item.item_condition_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Item condition can't be blank")
       end
       it 'freight_idが0では出品できない' do
-        @item.freight_id= 0
+        @item.freight_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Freight can't be blank")
       end
       it 'prefecture_idが0では出品できない' do
-        @item.prefecture_id= 0
+        @item.prefecture_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it 'ship_date_idが0では出品できない' do
-        @item.ship_date_id= 0
+        @item.ship_date_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Ship date can't be blank")
       end
       it 'priceが空では出品できない' do
-        @item.price= ''
+        @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it 'item_nameが41文字以上では出品できない' do
         @item.item_name = Faker::Lorem.characters(number: 41)
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item name is too long (maximum is 40 characters)")
+        expect(@item.errors.full_messages).to include('Item name is too long (maximum is 40 characters)')
       end
       it 'descriptionが1001文字以上では出品できない' do
         @item.description = Faker::Lorem.characters(number: 1001)
         @item.valid?
-        expect(@item.errors.full_messages).to include("Description is too long (maximum is 1000 characters)")
+        expect(@item.errors.full_messages).to include('Description is too long (maximum is 1000 characters)')
       end
       it 'priceが299以下では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is out of setting range")
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
       it 'priceが10,000,000以上では出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is out of setting range")
+        expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
       it 'priceに半角数字以外が含まれているときは出品できない' do
         @item.price = '１０００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters")
+        expect(@item.errors.full_messages).to include('Price is invalid. Input half-width characters')
       end
       it 'Userが紐づいていないときは出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
