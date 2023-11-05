39fac2ebbaa7dcb1b970f1b63851ae41205da2f2
K-Yaman0 <tezza10yama@gmail.com>
2023-11-05 22:14:07 +0900

    自身が出品した商品購入ページには遷移できない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,9 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    if user_signed_in? && current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 
 end
