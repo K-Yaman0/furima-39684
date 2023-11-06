9881b0c1daf5cea77180128be08e0a27655fe178
K-Yaman0 <tezza10yama@gmail.com>
2023-11-06 18:50:29 +0900

    コード入力ミス修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items, only: [:index, :new, :create, :edit, :update, :destroy, :show] do
-    resources :orders, only: [:index、:create]
+    resources :orders, only: [:index, :create]
   end
 end
