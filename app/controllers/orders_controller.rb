8e9cf1665f27e3bcc1504899a6edcbbe5e6d2f75
K-Yaman0 <tezza10yama@gmail.com>
2023-11-06 11:25:25 +0900

    ログアウト状態の時は商品購入ページに遷移できない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,12 @@
 class OrdersController < ApplicationController
 
   def index
-    @item = Item.find(params[:item_id])
+    if user_signed_in?
+      @item = Item.find(params[:item_id])
+    else
+      redirect_to root_path
+    end
+
     if user_signed_in? && current_user.id == @item.user.id
       redirect_to root_path
     end
