343d11f07b9b46c58de7e5532ce000543ca07f58
K-Yaman0 <tezza10yama@gmail.com>
2023-11-08 16:05:07 +0900

    コード修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,7 +13,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -12,8 +12,6 @@ class OrderAddress
   validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
 
-  belongs_to :order
-
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building,phone_number: phone_number, order_id: order.id )
