67f2ca248c873e08b5350293cdbacd16322285a0
K-Yaman0 <tezza10yama@gmail.com>
2023-11-14 23:45:25 +0900

    token以外(カード情報未実装)のエラーメッセージ表示

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,15 +5,15 @@ class OrderAddress
                 :building, :phone_number, :item_id, :user_id, :order_id
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :block
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input only number"}
     validates :user_id
     validates :item_id
   end
-  
+
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id )
