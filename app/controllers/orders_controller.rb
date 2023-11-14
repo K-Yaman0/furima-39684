41b8deaa1c67f6900838f45ebcdb3439fc49197b
K-Yaman0 <tezza10yama@gmail.com>
2023-11-14 23:01:47 +0900

    購入情報保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,16 +3,30 @@ class OrdersController < ApplicationController
   def index
     if user_signed_in?
       @item = Item.find(params[:item_id])
+      redirect_to root_path if current_user.id == @item.user.id
     else
       redirect_to root_path
     end
 
-    if user_signed_in? && current_user.id == @item.user.id
-      redirect_to root_path
-    end
+    @order_address = OrderAddress.new
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,20 +1,22 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city ,:block,
+  attr_accessor :postal_code, :prefecture_id, :city, :block,
                 :building, :phone_number, :item_id, :user_id, :order_id
 
-  [:postal_code, :prefecture_id, :city ,:block, :building, :phone_number].each do |v|
-    validates v, presence: true
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :block
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :user_id
+    validates :item_id
   end
-
-  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
-
+  
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building,phone_number: phone_number, order_id: order.id )
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, order_id: order.id )
   end
   
 end
