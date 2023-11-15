14e844b70b50ac95af64648a88abfa2067c70c6f
K-Yaman0 <tezza10yama@gmail.com>
2023-11-16 00:11:51 +0900

    ログインかつ自身の出品でも売却済みの商品編集ページに遷移できない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return if user_signed_in? && current_user.id == @item.user.id
+    return if user_signed_in? && current_user.id == @item.user.id && !@item.order.present?
 
     redirect_to action: :index
   end
