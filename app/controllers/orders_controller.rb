9bdf83cb204551ac4e44362d7004d587e2dea218
K-Yaman0 <tezza10yama@gmail.com>
2023-11-17 12:23:02 +0900

    コードをまとめる記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,14 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_order, only: [:index, :create]
 
   def index
-    if user_signed_in?
-      @item = Item.find(params[:item_id])
-      redirect_to root_path if current_user.id == @item.user.id
-    else
-      redirect_to root_path
-    end
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user.id
 
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
 
     if @order_address.valid?
@@ -30,4 +25,7 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def set_order
+    @item = Item.find(params[:item_id])
+  end
 end
